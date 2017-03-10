98f5f41fde935ecc0f5fc0a3ea90a413654c5a27
= <mattlink570@gmail.com>
2017-03-10 01:29:20 -0600

    added lib for functions, ticker printing

---
+++ lib.py
@@ -0,0 +1,11 @@
+
+## 
+# Various Useful Functions
+##
+
+def pticker(exchange, price, currency, time):
+	print "\n"
+	print "exchange: " + exchange
+	print "price: " + price	
+	print "currency: " + currency
+	print "time: " + time
