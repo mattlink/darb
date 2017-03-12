6b45644b32e6998222bc5105af18c4f300cba52f
Matt Link <mattlink570@gmail.com>
2017-03-11 20:56:29 -0600

    re wrote exchange classes

--- exchanges.py
+++ exchanges.py
@@ -1,82 +1,91 @@
-import requests
-import pprint
-pp = pprint.PrettyPrinter(indent=4)
-
-import lib
+import funcs as f
 
 cur_pair = "BTC-USD"
 
 class GDAX:
 
-	def __init__(self):
-		self.api_url = "http://api.gdax.com"
-		self.name = "GDAX"
+    def __init__(self):
+        self.api_url = "http://api.gdax.com"
+        self.name = "GDAX"
+        self.info = self.ticker()
 
-	def ticker(self):
-		url = self.api_url + "/products/BTC-USD/ticker"
-		r = requests.get(url)
-		data = r.json()
-		
-		price = data['price']	
-		time = data['time']
-	
-		lib.pticker(self.name, price, cur_pair, time)
-		
-		return data
+    def ticker(self):
+        url = self.api_url + "/products/BTC-USD/ticker"
+        data = f.http_get(url)
+
+        price = data['price']
+        time = data['time']
+
+        return_data = {}
+        return_data['name'] = self.name
+        return_data['price'] = price
+        return_data['currency'] = cur_pair
+        return_data['time'] = time
+
+        return return_data
+
+    def verbose(self):
+        f.verbose(self.info)
+
 
 class KRAKEN:
 
-	def __init__(self):
-		self.api_url = "http://api.kraken.com"
-		self.name = "Kraken"
+    def __init__(self):
+        self.api_url = "http://api.kraken.com"
+        self.name = "Kraken"
+        self.info = self.ticker()
 
-	def get_time(self):
-		url = self.api_url + "/0/public/Time"		
-		r = requests.get(url)		
-		data = r.json()
-		
-		#return data['result']['unixtime']		
-		return data['result']['rfc1123']
- 
-	def ticker(self):
-		url = self.api_url + "/0/public/Ticker?pair=XBTUSD"
-		r = requests.get(url)
-		data = r.json()['result']['XXBTZUSD']
-		
-		price = data['c'][0]	
-		time = self.get_time()
-	
-		lib.pticker(self.name, price, cur_pair, time)
-		
-		return data
+    def get_time(self):
+        url = self.api_url + "/0/public/Time"
+        data = f.http_get(url)
 
-	def assets(self):
-		url = self.api_url + "/0/public/AssetPairs"
-		r = requests.get(url)
-		data = r.json()
-		
-		pp.pprint(data['result'])
+        ## can return unixtime with data['result']['unixtime']
+        return data['result']['rfc1123']
 
-class POLONIEX: 
-	
-	def __init__(self):
-		self.api_url = "http://poloniex.com/public?command="
-		self.name = "Poloniex"
+    def ticker(self):
+        url = self.api_url + "/0/public/Ticker?pair=XBTUSD"
+        data = f.http_get(url)
+        data = data['result']['XXBTZUSD']
 
-	def ticker(self):
-		url = self.api_url + "returnTicker"
-		r = requests.get(url)
-		data = r.json()
-		
-		# pp.pprint(data)
-		print "EXCHANGE: POLONIEX"
-		print "PRODUCT: BTC-USD"
-		print "DATA: " 
-		pp.pprint(data['USDT_BTC'])
-		print "TIME: " + "insert time"
-		
-		return data
-        
+        price = data['c'][0]
+        time = self.get_time()
+
+        return_data = {}
+        return_data['name'] = self.name
+        return_data['price'] = price
+        return_data['currency'] = cur_pair
+        return_data['time'] = time
+
+        return return_data
+
+    def verbose(self):
+        f.verbose(self.info)
+
+class POLONIEX:
+
+    def __init__(self):
+        self.api_url = "http://poloniex.com/public?command="
+        self.name = "Poloniex"
+        self.info = self.ticker()
+
+    def ticker(self):
+        url = self.api_url + "returnTicker"
+        data = f.http_get(url)
+
+        price = data['USDT_BTC']['last']
+        time = "time"
+
+        return_data = {}
+        return_data['name'] = self.name
+        return_data['price'] = price
+        return_data['currency'] = cur_pair
+        return_data['time'] = time
+
+        return return_data
+
+    def verbose(self):
+        f.verbose(self.info)
+
 
 if __name__ == "__main__":
 	print "the exchanges lib"
